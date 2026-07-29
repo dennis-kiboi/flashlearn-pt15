06d354d595787be1919d22e140cbedd24422346c
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2026-07-29 19:59:56 +0300

    add a post request to the form

--- js/script.js
+++ js/script.js
@@ -23,48 +23,53 @@ fetch("http://localhost:3000/flashcards")
   .then(res => res.json())
   .then(flashcards => renderFlashcards(flashcards));
 
-function renderFlashcards(flashcards) {
-  cardsContainer.innerHTML = "";
-  flashcards.forEach((card, index) => {
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
-    cardElement.innerHTML = `
+function createFlashcardElement(card) {
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("card");
+  cardElement.innerHTML = `
     <h3>${card.question}</h3>
     <p class="answer hidden">${card.answer}</p>
     <button class="show-btn">Show Answer</button>
     `;
 
-    const answer = cardElement.querySelector(".answer");
-    const showButton = cardElement.querySelector(".show-btn");
+  const answer = cardElement.querySelector(".answer");
+  const showButton = cardElement.querySelector(".show-btn");
 
-    // console.log(answer.classList.contains("hidden"))
+  // console.log(answer.classList.contains("hidden"))
 
+  // if (showButton.textContent === "Show Answer") {
+  //   showButton.textContent = "Hide Answer";
+  // } else {
+  //   showButton.textContent = "Show Answer";
+  // }
+
+  showButton.addEventListener("click", () => {
+    answer.classList.toggle("hidden");
+
+    // Uncomment one of the solutions to test
+
+    // Hope's solution
     // if (showButton.textContent === "Show Answer") {
     //   showButton.textContent = "Hide Answer";
     // } else {
     //   showButton.textContent = "Show Answer";
     // }
 
-    showButton.addEventListener("click", () => {
-      answer.classList.toggle("hidden");
+    // Dennis' Solution
+    showButton.textContent = answer.classList.contains("hidden")
+      ? "Show Answer"
+      : "Hide Answer";
 
-      // Uncomment one of the solutions to test
+    // condition ? value if true : value if false
+  });
 
-      // Hope's solution
-      // if (showButton.textContent === "Show Answer") {
-      //   showButton.textContent = "Hide Answer";
-      // } else {
-      //   showButton.textContent = "Show Answer";
-      // }
+  return cardElement;
+}
 
-      // Dennis' Solution
-      showButton.textContent = answer.classList.contains("hidden")
-        ? "Show Answer"
-        : "Hide Answer";
-
-      // condition ? value if true : value if false
-    });
-
+function renderFlashcards(flashcards) {
+  cardsContainer.innerHTML = "";
+  flashcards.forEach((card, index) => {
+    const cardElement = createFlashcardElement(card);
     cardsContainer.appendChild(cardElement);
   });
 }
@@ -74,11 +79,25 @@ form.addEventListener("submit", event => {
   const question = questionInput.value;
   const answer = answerInput.value;
 
-  flashcards.push({
-    question,
-    answer
-  });
+  // flashcards.push({
+  //   question,
+  //   answer
+  // });
 
-  renderFlashcards(); // re-render all cards
+  // add new flashcard using a post request
+  fetch("http://localhost:3000/flashcards", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ question, answer })
+  })
+    .then(res => res.json())
+    .then(createdFlashcard => {
+      // update the dom with the new createdFlashcard
+      cardsContainer.appendChild(createFlashcardElement(createdFlashcard));
+    });
+
+  // renderFlashcards(); // re-render all cards
   form.reset();
 });
