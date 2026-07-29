9fb692e65cc34025bf9b50337e7f4012eff3020a
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2026-07-29 20:06:57 +0300

    clean up the js file

--- js/script.js
+++ js/script.js
@@ -3,26 +3,7 @@ const questionInput = document.querySelector("#question");
 const answerInput = document.querySelector("#answer");
 const cardsContainer = document.querySelector("#cards-container");
 
-// const flashcards = [
-//   {
-//     question: "What does HTML stand for?",
-//     answer: "HyperText Markup Language"
-//   },
-//   {
-//     question: "What does CSS stand for?",
-//     answer: "Cascading Styles Sheets"
-//   },
-//   {
-//     question: "What language makes web pages interactive",
-//     answer: "JavaScript"
-//   }
-// ];
-
-// get data using fetch
-fetch("http://localhost:3000/flashcards")
-  .then(res => res.json())
-  .then(flashcards => renderFlashcards(flashcards));
-
+// Function to create a single flashcard element from a flashcard object
 function createFlashcardElement(card) {
   const cardElement = document.createElement("div");
   cardElement.classList.add("card");
@@ -35,54 +16,33 @@ function createFlashcardElement(card) {
   const answer = cardElement.querySelector(".answer");
   const showButton = cardElement.querySelector(".show-btn");
 
-  // console.log(answer.classList.contains("hidden"))
-
-  // if (showButton.textContent === "Show Answer") {
-  //   showButton.textContent = "Hide Answer";
-  // } else {
-  //   showButton.textContent = "Show Answer";
-  // }
-
   showButton.addEventListener("click", () => {
     answer.classList.toggle("hidden");
 
-    // Uncomment one of the solutions to test
-
-    // Hope's solution
-    // if (showButton.textContent === "Show Answer") {
-    //   showButton.textContent = "Hide Answer";
-    // } else {
-    //   showButton.textContent = "Show Answer";
-    // }
-
-    // Dennis' Solution
     showButton.textContent = answer.classList.contains("hidden")
       ? "Show Answer"
       : "Hide Answer";
-
-    // condition ? value if true : value if false
   });
 
   return cardElement;
 }
 
-function renderFlashcards(flashcards) {
-  cardsContainer.innerHTML = "";
-  flashcards.forEach((card, index) => {
-    const cardElement = createFlashcardElement(card);
-    cardsContainer.appendChild(cardElement);
+// Initial fetch request on page load
+fetch("http://localhost:3000/flashcards")
+  .then(res => res.json())
+  .then(flashcards => {
+    cardsContainer.innerHTML = "";
+    flashcards.forEach((card, index) => {
+      const cardElement = createFlashcardElement(card);
+      cardsContainer.appendChild(cardElement);
+    });
   });
-}
 
+// Form submission handler
 form.addEventListener("submit", event => {
   event.preventDefault();
   const question = questionInput.value;
   const answer = answerInput.value;
-
-  // flashcards.push({
-  //   question,
-  //   answer
-  // });
 
   // add new flashcard using a post request
   fetch("http://localhost:3000/flashcards", {
@@ -98,6 +58,5 @@ form.addEventListener("submit", event => {
       cardsContainer.appendChild(createFlashcardElement(createdFlashcard));
     });
 
-  // renderFlashcards(); // re-render all cards
   form.reset();
 });
